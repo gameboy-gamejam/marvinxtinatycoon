ef85a910ff5ea9d9dd78a3a9a5fe18a76317dcbc
Paulo Carabuena <jkishido@gmail.com>
2013-12-29 14:10:27 +0800

    initial factory class

---
+++ GameJam/src/com/gj/gb/factory/GBIngredientsFactory.java
@@ -0,0 +1,12 @@
+package com.gj.gb.factory;
+
+import com.gj.gb.model.GBIngredient;
+
+public class GBIngredientsFactory {
+
+	public static final GBIngredient getIngredientById(int id) {
+		switch (id) {
+		default: return null;
+		}
+	}
+}
---
+++ GameJam/src/com/gj/gb/factory/GBRecipeFactory.java
@@ -0,0 +1,13 @@
+package com.gj.gb.factory;
+
+import com.gj.gb.model.GBRecipe;
+
+public class GBRecipeFactory {
+
+	public static final GBRecipe getRecipeById(int id) {
+		
+		switch (id) {
+		default: return null;
+		}
+	}
+}
