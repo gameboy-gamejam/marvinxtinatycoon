24c8a7c2de56b585532b4fd1fb43061a28978954
Paulo Carabuena <jkishido@gmail.com>
2013-12-29 18:13:44 +0800

    load bitmap from specified prefix and id

--- GameJam/src/com/gj/gb/util/Utils.java
+++ GameJam/src/com/gj/gb/util/Utils.java
@@ -2,8 +2,17 @@ package com.gj.gb.util;
 
 import java.util.Random;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+
 public class Utils {
 
 	public static final Random RANDOM = new Random(System.currentTimeMillis());
 	
+	public static Bitmap getBitmapFromDrawable(Context context, String prefix, int id) {
+		int resid = context.getResources().getIdentifier(prefix+"_"+id, "drawable", context.getPackageName());
+		BitmapDrawable drawable = (BitmapDrawable) context.getResources().getDrawable(resid);
+		return drawable.getBitmap();
+	}
 }
