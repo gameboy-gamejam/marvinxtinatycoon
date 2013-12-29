a284920298ed4623f04e9c8ca3d59871fb6fdbd1
Paulo Carabuena <jkishido@gmail.com>
2013-12-29 18:26:24 +0800

    update getBitmapFromDrawable

--- GameJam/src/com/gj/gb/util/Utils.java
+++ GameJam/src/com/gj/gb/util/Utils.java
@@ -11,7 +11,11 @@ public class Utils {
 	public static final Random RANDOM = new Random(System.currentTimeMillis());
 	
 	public static Bitmap getBitmapFromDrawable(Context context, String prefix, int id) {
-		int resid = context.getResources().getIdentifier(prefix+"_"+id, "drawable", context.getPackageName());
+		return getBitmapFromDrawable(context, prefix+"_"+id);
+	}
+	
+	public static Bitmap getBitmapFromDrawable(Context context, String name) {
+		int resid = context.getResources().getIdentifier(name, "drawable", context.getPackageName());
 		BitmapDrawable drawable = (BitmapDrawable) context.getResources().getDrawable(resid);
 		return drawable.getBitmap();
 	}
