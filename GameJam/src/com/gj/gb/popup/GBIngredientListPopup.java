6a5461bfba178dee9998462e97a59cdfc60460d8
Gadwin <marvinmosa@gmail.com>
2013-12-30 14:11:45 +0800

    added the add and sub functions

--- GameJam/src/com/gj/gb/popup/GBIngredientListPopup.java
+++ GameJam/src/com/gj/gb/popup/GBIngredientListPopup.java
@@ -2,21 +2,23 @@ package com.gj.gb.popup;
 
 import java.util.List;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+
 import com.gj.gb.R;
 import com.gj.gb.factory.GBIngredientsFactory;
 import com.gj.gb.factory.GBRecipeFactory;
 import com.gj.gb.model.GBIngredient;
 import com.gj.gb.model.GBRecipe;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
-
 public class GBIngredientListPopup extends Activity {
 	private int mDishId;
+	private int mCount;
 	GBRecipe mRecipe;
 
 	private static int MAX_OVEN_CAPACITY = 5;
@@ -28,29 +30,31 @@ public class GBIngredientListPopup extends Activity {
 	private int[] mMaxIdList = new int[] { R.id.max_ingredient_1,
 			R.id.max_ingredient_2, R.id.max_ingredient_3 };
 
+	private int[] mCurrentIdList = new int[] { R.id.current_ingredient_1,
+			R.id.current_ingredient_2, R.id.current_ingredient_3 };
+
 	SharedPreferences mIngredientCountPref;
+	List<Integer> mIngredientList;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		List<Integer> mIngredientList;
 		setContentView(R.layout.popup_ingredient_list);
 
-		//get saved ingredient count
-		mIngredientCountPref = getSharedPreferences("IngredientCounts", MODE_PRIVATE);
-		
-		//get intent
+		// get saved ingredient count
+		mIngredientCountPref = getSharedPreferences("IngredientCounts",
+				MODE_PRIVATE);
+
+		// get intent
 		Intent intent = getIntent();
 		mDishId = intent.getIntExtra("DishId", -1);
+		mCount = 0;
 		GBRecipe mRecipe = GBRecipeFactory.getRecipeById(mDishId);
 		mIngredientList = mRecipe.getIngredients();
 
-		TextView dishName = (TextView) findViewById(R.id.name);
-
-		TextView maxOvenCapacity = (TextView) findViewById(R.id.max_value);
-
-		dishName.setText(mRecipe.getName());
-		maxOvenCapacity.setText(MAX_OVEN_CAPACITY + "");
+		((TextView) findViewById(R.id.name)).setText(mRecipe.getName());
+		((TextView) findViewById(R.id.max_value)).setText(MAX_OVEN_CAPACITY
+				+ "");
 
 		// hide all the view first
 		for (int i = 0; i < mHolderIdList.length; i++) {
@@ -62,7 +66,7 @@ public class GBIngredientListPopup extends Activity {
 			findViewById(mHolderIdList[i]).setVisibility(View.VISIBLE);
 			GBIngredient ingredient = GBIngredientsFactory
 					.getIngredientById(mIngredientList.get(i));
-			
+
 			((TextView) findViewById(mNameIdList[i])).setText(ingredient
 					.getName());
 
@@ -72,6 +76,47 @@ public class GBIngredientListPopup extends Activity {
 							+ "");
 
 		}
+		
+		findViewById(R.id.btn_close).setOnClickListener( new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
+		
+		findViewById(R.id.btn_right).setOnClickListener( new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				addCount();
+			}
+		});
+		
+		findViewById(R.id.btn_left).setOnClickListener( new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				subCount();
+			}
+		});
 
+	}
+
+	public void addCount() {
+		if (mCount >= MAX_OVEN_CAPACITY)
+			return;
+		mCount++;
+		for (int i = 0; i < mIngredientList.size(); i++) {
+			((TextView) findViewById(mCurrentIdList[i])).setText(mCount + "");
+		}
+		((TextView) findViewById(R.id.current_value)).setText(mCount + "");
+	}
+	
+	public void subCount() {
+		if (mCount <= 0)
+			return;
+		mCount--;
+		for (int i = 0; i < mIngredientList.size(); i++) {
+			((TextView) findViewById(mCurrentIdList[i])).setText(mCount + "");
+		}
+		((TextView) findViewById(R.id.current_value)).setText(mCount + "");
 	}
 }
