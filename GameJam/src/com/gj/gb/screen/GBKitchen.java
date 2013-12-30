9b71c2244484da9bce6d30416aab43a364c6ab4f
Gadwin <marvinmosa@gmail.com>
2013-12-30 17:19:06 +0800

    linked everything

--- GameJam/src/com/gj/gb/screen/GBKitchen.java
+++ GameJam/src/com/gj/gb/screen/GBKitchen.java
@@ -1,9 +1,13 @@
 package com.gj.gb.screen;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 import com.gj.gb.R;
+import com.gj.gb.popup.GBDishListPopup;
 
 public class GBKitchen extends Activity {
 
@@ -15,6 +19,41 @@ public class GBKitchen extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.scene_kitchen);
 
+		findViewById(R.id.btn_cook_1).setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				toDishList(1);
+			}
+		});
+		
+		findViewById(R.id.btn_cook_2).setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				toDishList(2);
+			}
+		});
+
+	}
+	
+	private void toDishList(int stove_no){
+		Intent intent = new Intent(this, GBDishListPopup.class);
+		intent.putExtra("stove_no", stove_no);
+		startActivityForResult(intent, REQUEST_CODE_DISH);
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		switch (requestCode) {
+		case REQUEST_CODE_DISH:
+			if (resultCode == RESULT_CODE_DISH) {
+				//TODO
+			}
+
+		}
 	}
 
 }
