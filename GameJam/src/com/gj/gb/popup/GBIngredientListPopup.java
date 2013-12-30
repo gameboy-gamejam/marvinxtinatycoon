db6896c5c4d361385e5776e9fa7c51b8eee7a4ce
Gadwin <marvinmosa@gmail.com>
2013-12-30 11:49:31 +0800

    updated ingredient popup

--- GameJam/src/com/gj/gb/popup/GBIngredientListPopup.java
+++ GameJam/src/com/gj/gb/popup/GBIngredientListPopup.java
@@ -10,7 +10,7 @@ public class GBIngredientListPopup extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.scene_splash);
+        setContentView(R.layout.popup_ingredient_list);
 
     }
 }
