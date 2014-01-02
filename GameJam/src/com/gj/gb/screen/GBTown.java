164ad2d5a7d73ac8d8808faf881934762b2b22f6
Juan Paulo Carabuena <carabuena.paulo@klab.com>
2014-01-02 10:01:13 +0800

    update town screen and added activities to manifest

--- GameJam/src/com/gj/gb/screen/GBTown.java
+++ GameJam/src/com/gj/gb/screen/GBTown.java
@@ -10,8 +10,7 @@ public class GBTown extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.scene_splash);
+        setContentView(R.layout.scene_town);
 
     }
 }
