23a61d458552cdf96480294f02fdff103e3cc98d
Juan Paulo Carabuena <carabuena.paulo@klab.com>
2014-01-03 12:19:53 +0800

    user ingredient list in market

---
+++ GameJam/src/com/gj/gb/gridview/IngredientMarketGridViewAdapter.java
@@ -0,0 +1,59 @@
+package com.gj.gb.gridview;
+
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.gj.gb.R;
+import com.gj.gb.logic.GBEconomics;
+import com.gj.gb.model.GBIngredient;
+
+public class IngredientMarketGridViewAdapter extends ArrayAdapter<GBIngredient> {
+
+	Context context;
+	List<GBIngredient> data;
+	
+	public IngredientMarketGridViewAdapter(Context context, List<GBIngredient> objects) {
+		super(context, R.layout.part_ingredient_item, R.id.textDummy, objects);
+		
+		this.context = context;
+		this.data = objects;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		IngredientHolder holder = null;
+		
+		if (convertView == null) {
+			LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+			convertView = inflater.inflate(R.layout.part_ingredient_item, parent, false);
+			
+			holder = new IngredientHolder();
+			holder.imageIcon = (ImageView) convertView.findViewById(R.id.imageIngredientIcon);
+			holder.textPrice = (TextView) convertView.findViewById(R.id.textRarity); // reuse na lang
+			
+			convertView.setTag(holder);
+		} else {
+			holder = (IngredientHolder) convertView.getTag();
+		}
+		
+		GBIngredient ingredient = getItem(position);
+
+		holder.textPrice.setText(GBEconomics.recomputePrice(ingredient.getPrice()) + "G");
+		holder.textPrice.setTextColor(GBEconomics.getRateColor());
+		
+		return super.getView(position, convertView, parent);
+	}
+	
+	static class IngredientHolder {
+		TextView textPrice;
+		ImageView imageIcon;
+	}
+}
--- GameJam/src/com/gj/gb/screen/GBMarket.java
+++ GameJam/src/com/gj/gb/screen/GBMarket.java
@@ -1,22 +1,29 @@
 package com.gj.gb.screen;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.GridView;
+import android.widget.TextView;
+
 import com.gj.gb.R;
+import com.gj.gb.gridview.IngredientMarketGridViewAdapter;
 import com.gj.gb.model.GBGameData;
 import com.gj.gb.util.GBDataManager;
 import com.gj.gb.util.Utils;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.TextView;
-
 public class GBMarket extends Activity {
 
+	private GBGameData data;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.scene_market);
+		
+		data = GBDataManager.getGameData();
 		
 		initData();
 		initButtons();
@@ -30,10 +37,13 @@ public class GBMarket extends Activity {
 			
 			switch (id) {
 			case R.id.buttonDone:
-				finish();
+				confirmExit();
 				break;
 			case R.id.buttonBuy:
+				break;
 			case R.id.buttonSell:
+				updateGridView();
+				break;
 			}
 		}
 	};
@@ -44,10 +54,31 @@ public class GBMarket extends Activity {
 		findViewById(R.id.buttonSell).setOnClickListener(buttonListener);
 	}
 
+	protected void confirmExit() {
+		Intent intent = new Intent(this, GBPopConfirm.class);
+		intent.putExtra("message", "Are you finished buying/selling ingredients?");
+		startActivityForResult(intent, 100);
+	}
+
+	protected void updateGridView() {
+		GridView grid = (GridView) findViewById(R.id.gridList);
+		grid.setAdapter(new IngredientMarketGridViewAdapter(this, data.getIngredients()));
+	}
+
 	private void initData() {
 		GBGameData data = GBDataManager.getGameData();
 		
 		((TextView) findViewById(R.id.textCurrentGold)).setText(Utils.formatNum(data.getCurrentGold(), "#,###,###"));
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		
+		if (requestCode == 100 && resultCode == RESULT_OK) {
+			setResult(RESULT_OK);
+			finish();
+		}
 	}
 
 }
