ade631fc186ac99e9f51377fb4984afc311274b2
Juan Paulo Carabuena <carabuena.paulo@klab.com>
2014-01-03 11:50:40 +0800

    added ingredients grid list

---
+++ GameJam/src/com/gj/gb/gridview/IngredientGridViewAdapter.java
@@ -0,0 +1,57 @@
+package com.gj.gb.gridview;
+
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.gj.gb.R;
+import com.gj.gb.model.GBIngredient;
+
+public class IngredientGridViewAdapter extends ArrayAdapter<GBIngredient> {
+
+	Context context;
+	List<GBIngredient> data;
+	
+	public IngredientGridViewAdapter(Context context, List<GBIngredient> objects) {
+		super(context, R.layout.part_ingredient_item, R.id.textDummy, objects);
+		
+		this.context = context;
+		this.data = objects;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		IngredientHolder holder = null;
+		
+		if (convertView == null) {
+			LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+			convertView = inflater.inflate(R.layout.part_ingredient_item, parent, false);
+			
+			holder = new IngredientHolder();
+			holder.imageIcon = (ImageView) convertView.findViewById(R.id.imageIngredientIcon);
+			holder.textRarity = (TextView) convertView.findViewById(R.id.textRarity);
+			
+			convertView.setTag(holder);
+		} else {
+			holder = (IngredientHolder) convertView.getTag();
+		}
+		
+		GBIngredient ingredient = getItem(position);
+
+		holder.textRarity.setText(ingredient.getRarity() + "☆");
+		
+		return super.getView(position, convertView, parent);
+	}
+	
+	static class IngredientHolder {
+		TextView textRarity;
+		ImageView imageIcon;
+	}
+}
--- GameJam/src/com/gj/gb/screen/GBCommonGridList.java
+++ GameJam/src/com/gj/gb/screen/GBCommonGridList.java
@@ -5,15 +5,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+import android.widget.TextView;
 
 import com.gj.gb.R;
+import com.gj.gb.gridview.IngredientGridViewAdapter;
+import com.gj.gb.model.GBGameData;
+import com.gj.gb.model.GBIngredient;
+import com.gj.gb.util.GBDataManager;
 
 public class GBCommonGridList extends Activity {
 
+	protected GBGameData data;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
+		data = GBDataManager.getGameData();
+		
 		int type = getIntent().getIntExtra("type", -1);
 		setContent(type);
 		
@@ -31,6 +43,17 @@ public class GBCommonGridList extends Activity {
 		switch (type) {
 		case R.id.buttonIngredients:
 			setContentView(R.layout.scene_ingredients_grid);
+			GridView grid = (GridView) findViewById(R.id.gridList);
+			grid.setAdapter(new IngredientGridViewAdapter(this, data.getIngredients()));
+			grid.setOnItemClickListener(new OnItemClickListener() {
+
+				@Override
+				public void onItemClick(AdapterView<?> parent, View view,
+						int position, long id) {
+					updateIngredientInfo(position);
+				}
+			});
+			updateIngredientInfo(0);
 			break;
 		case R.id.buttonRecipe:
 			setContentView(R.layout.scene_recipe_grid);
@@ -41,6 +64,14 @@ public class GBCommonGridList extends Activity {
 		default:
 			finish();
 		}
+	}
+
+	protected void updateIngredientInfo(int position) {
+		GBIngredient ingredient = data.getIngredients().get(position);
+		
+		((TextView) findViewById(R.id.textIngredientName)).setText(ingredient.getName());
+		((TextView) findViewById(R.id.textIngredientPrice)).setText(""+ingredient.getPrice());
+		((TextView) findViewById(R.id.textIngredientQty)).setText(ingredient.getQuantity()+"x");
 	}
 
 	@Override
