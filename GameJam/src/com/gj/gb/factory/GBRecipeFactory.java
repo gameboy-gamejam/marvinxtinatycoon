1a0a223ef8d816d817c2596382028b2dd5144435
Gadwin <marvinmosa@gmail.com>
2014-01-04 23:37:16 +0800

    in progress

--- GameJam/src/com/gj/gb/factory/GBRecipeFactory.java
+++ GameJam/src/com/gj/gb/factory/GBRecipeFactory.java
@@ -13,36 +13,35 @@ public class GBRecipeFactory {
 
 		switch (id) {
 		case 0:
-			return new GBRecipe(id, "72OZ STEAK", "Sample", 6, 6, 6);
+			return new GBRecipe(id, "72OZ STEAK", "A steak is generally a cut of beef sliced perpendicular to the muscle fibers, or of fish cut perpendicular to the spine.", 6, 6, 6);
 		case 1:
-			return new GBRecipe(id, "APPLE TARTIN", "Sample", 0, 19);
+			return new GBRecipe(id, "APPLE TARTIN", "Is an upside-down tart in which the fruit (usually apples) are caramelized in butter and sugar before the tart is baked.", 0, 19);
 		case 2:
-			return new GBRecipe(id, "BANANA SPLIT", "Sample", 2, 19);
+			return new GBRecipe(id, "BANANA SPLIT", "Is an ice cream-based dessert. In its classic form it is served in a long dish called a boat.", 2, 19);
 		case 3:
-			return new GBRecipe(id, "BEANS AND MUSHROOM NOODLES", "Sample", 5,
+			return new GBRecipe(id, "BEANS AND MUSHROOM NOODLES", "A simple noode.", 5,
 					26, 27);
 		case 4:
-			return new GBRecipe(id, "BEEF ENCHILADA", "Sample", 13, 11, 6);
+			return new GBRecipe(id, "BEEF ENCHILADA", "Round beef and onion are wrapped in flour tortillas, topped with Cheddar cheese and black olives, then baked.", 13, 11, 6);
 		case 5:
-			return new GBRecipe(id, "BISCOTTI", "Sample", 16, 14);
+			return new GBRecipe(id, "BISCOTTI", "Are twice-baked cookies (or biscuits) originating in the Italian city of Prato.", 16, 14);
 		case 6:
-			return new GBRecipe(id, "BLEU VAMPIRE STEAK", "Sample", 6, 33, 44);
+			return new GBRecipe(id, "BLEU VAMPIRE STEAK", "A very expensive steak.", 6, 33, 44);
 		case 7:
-			return new GBRecipe(id, "BRIGADERIOS", "Sample", 14, 8, 25);
+			return new GBRecipe(id, "BRIGADERIOS", "Is a simple Brazilian chocolate bonbon.", 14, 8, 25);
 		case 8:
-			return new GBRecipe(id, "BRUSCHETTA WITH TOMATO AND BASIL",
-					"Sample", 7, 44, 3);
+			return new GBRecipe(id, "BRUSCHETTA WITH TOMATO AND BASIL", "Chopped fresh tomatoes with garlic, basil, olive oil, and vinegar, served on toasted slices of French.", 7, 44, 3);
 		case 9:
-			return new GBRecipe(id, "BURGER AND FRIES", "Sample", 7, 6, 33);
+			return new GBRecipe(id, "BURGER AND FRIES", "A common snack.", 7, 6, 33);
 		case 10:
 			return new GBRecipe(id, "CABBAGE ROLLS", "Sample", 36, 6, 9);
 		case 11:
-			return new GBRecipe(id, "CAESAR SALAD", "Sample", 38, 7, 16);
+			return new GBRecipe(id, "CAESAR SALAD", "A common salad.", 38, 7, 16);
 		case 12:
-			return new GBRecipe(id, "CAMEMBERT BAKED IN A BOX", "Sample", 11,
+			return new GBRecipe(id, "CAMEMBERT BAKED IN A BOX", " Vacherin, the deep, deliciously smelly cheese, is the usual one for baking whole in its box.", 11,
 					11, 7);
 		case 13:
-			return new GBRecipe(id, "CARIBBEAN CHICKEN SALAD", "Sample", 12, 5,
+			return new GBRecipe(id, "CARIBBEAN CHICKEN SALAD", "Work that skillet with the chicken and seasonings, and you'll cruise upon Island flavors to brag about without leaving home.", 12, 5,
 					38);
 		case 14:
 			return new GBRecipe(id, "CHEESE BOARD", "Sample", 11, 11, 11);
@@ -169,98 +168,97 @@ public class GBRecipeFactory {
 		case 72:
 			return new GBRecipe(id, "RIB 'N' WINGS", "Sample", 32, 12, 42);
 		case 73:
-			return new GBRecipe(id, "ROAST BEEF", "Sample", 6, 10, 38);
+			return new GBRecipe(id, "ROAST BEEF", "A roasted beef.", 6, 10, 38);
 		case 74:
-			return new GBRecipe(id, "ROAST CHICKEN", "Sample", 12, 33, 38);
+			return new GBRecipe(id, "ROAST CHICKEN", "A roasted chicken.", 12, 33, 38);
 		case 75:
-			return new GBRecipe(id, "SAKURA MOCHI", "Sample", 5, 36);
+			return new GBRecipe(id, "SAKURA MOCHI", "Is a variety of wagashi, or Japanese confectionery consisting of a sweet pink mochi", 5, 36);
 		case 76:
-			return new GBRecipe(id, "SALMON HANDROLLS", "Sample", 39, 36, 40);
+			return new GBRecipe(id, "SALMON HANDROLLS", "This tasty sushi roll is the perfect way to make use of that leftover salmon skin.", 39, 36, 40);
 		case 77:
-			return new GBRecipe(id, "SALMON KEBABS WITH CHILI LIME GLAZE",
-					"Sample", 39, 23, 13);
+			return new GBRecipe(id, "SALMON KEBABS WITH CHILI LIME GLAZE", "A very expensive dish.", 39, 23, 13);
 		case 78:
-			return new GBRecipe(id, "SALMON SASHIMI", "Sample", 39, 39, 46);
+			return new GBRecipe(id, "SALMON SASHIMI", "A Japanese delicacy consisting of very fresh raw meat or fish sliced into thin ", 39, 39, 46);
 		case 79:
-			return new GBRecipe(id, "SALMON SUSHI", "Sample", 36, 39, 46);
+			return new GBRecipe(id, "SALMON SUSHI", "A salmon placed on a sushi.", 36, 39, 46);
 		case 80:
-			return new GBRecipe(id, "SCHINTZEL", "Sample", 32, 7, 44);
+			return new GBRecipe(id, "SCHINTZEL", "A boneless meat, thinned with a hammer (meat tenderizer), coated with flour, beaten eggs and bread crumbs, and then fried.", 32, 7, 44);
 		case 81:
-			return new GBRecipe(id, "SCRAMBLED EGGS", "Sample", 16, 8);
+			return new GBRecipe(id, "SCRAMBLED EGGS", "An egg dish.", 16, 8);
 		case 82:
-			return new GBRecipe(id, "SEAFOOD CHOWDER", "Sample", 39, 15, 25);
+			return new GBRecipe(id, "SEAFOOD CHOWDER", "Lovely, thick and creamy chowder,", 39, 15, 25);
 		case 83:
-			return new GBRecipe(id, "SEAFOOD PAELLA", "Sample", 34, 36, 37);
+			return new GBRecipe(id, "SEAFOOD PAELLA", "A thin blanket of saffron-scented  rice with a crackly, crunchy bottom crust (or socarrat), studded with fresh seafood.", 34, 36, 37);
 		case 84:
-			return new GBRecipe(id, "SEAFOOD PASTA", "Sample", 29, 34, 45);
+			return new GBRecipe(id, "SEAFOOD PASTA", "A seafood flavored pasta.", 29, 34, 45);
 		case 85:
-			return new GBRecipe(id, "SEAFOOD PLATTER", "Sample", 39, 15, 24);
+			return new GBRecipe(id, "SEAFOOD PLATTER", "A platter filled with assorted seafoods.", 39, 15, 24);
 		case 86:
-			return new GBRecipe(id, "SEAWEED SALAD", "Sample", 40, 18, 13);
+			return new GBRecipe(id, "SEAWEED SALAD", "A healthy Japanese dish", 40, 18, 13);
 		case 87:
-			return new GBRecipe(id, "SHRIMP ON THE BARBIE", "Sample", 34, 22,
+			return new GBRecipe(id, "SHRIMP ON THE BARBIE", "A very exotic dish.", 34, 22,
 					17);
 		case 88:
-			return new GBRecipe(id, "SMOKED SALMON PLATTER", "Sample", 39, 39,
+			return new GBRecipe(id, "SMOKED SALMON PLATTER", " Smoked salmon with all the accoutrements, chopped onions, capers, sour cream, and chopped egg whites.", 39, 39,
 					22);
 		case 89:
-			return new GBRecipe(id, "SOUR STRAWBERRY SWIRLIES", "Sample", 23,
+			return new GBRecipe(id, "SOUR STRAWBERRY SWIRLIES", "A simple dessert.", 23,
 					41, 22);
 		case 90:
-			return new GBRecipe(id, "SPAGHETTI BOLOGNESE", "Sample", 29, 44, 6);
+			return new GBRecipe(id, "SPAGHETTI BOLOGNESE", "A classic bolognese sauce still tastes great, no matter how simple.", 29, 44, 6);
 		case 91:
-			return new GBRecipe(id, "SPAGHETTI CARBONARA", "Sample", 29, 6, 16);
+			return new GBRecipe(id, "SPAGHETTI CARBONARA", "Is one of the most popular Italian pasta dishes. ", 29, 6, 16);
 		case 92:
-			return new GBRecipe(id, "SPICY BUFFALO WINGS", "Sample", 12, 8, 13);
+			return new GBRecipe(id, "SPICY BUFFALO WINGS", "It is traditionally fried unbreaded and then coated in sauce.", 12, 8, 13);
 		case 93:
-			return new GBRecipe(id, "STEAK AND CHIPS", "Sample", 6, 6, 33);
+			return new GBRecipe(id, "STEAK AND CHIPS", "Steak and chips may seem a bit basic but done correctly, this dish is perfection.", 6, 6, 33);
 		case 94:
-			return new GBRecipe(id, "STRAWBERRY CAKE", "Sample", 8, 41);
+			return new GBRecipe(id, "STRAWBERRY CAKE", "A strawberry cake with a layer of cheesecake hidden in the middle and covered with strawberry.", 8, 41);
 		case 95:
-			return new GBRecipe(id, "STRAWBERRY CHEESECAKE", "Sample", 11, 41);
+			return new GBRecipe(id, "STRAWBERRY CHEESECAKE", "Made with cream cheese and a touch of lemon that's covered with a fresh, sweet, chunky strawberry sauce.", 11, 41);
 		case 96:
-			return new GBRecipe(id, "STRAWBERRY SUNDAE", "Sample", 19, 41, 41);
+			return new GBRecipe(id, "STRAWBERRY SUNDAE", "A straberry flavored ice cream.", 19, 41, 41);
 		case 97:
-			return new GBRecipe(id, "SUNDAE", "Sample", 14, 19);
+			return new GBRecipe(id, "SUNDAE", "A sweet ice cream dessert", 14, 19);
 		case 98:
-			return new GBRecipe(id, "SWEET RICE CAKE", "Sample", 36, 36);
+			return new GBRecipe(id, "SWEET RICE CAKE", "A Japanese cake made from glutinous rice.", 36, 36);
 		case 99:
-			return new GBRecipe(id, "SWEETCORN SOUP", "Sample", 42, 16);
+			return new GBRecipe(id, "SWEETCORN SOUP", "A combination of sweetcorn and normal soup.", 42, 16);
 		case 100:
-			return new GBRecipe(id, "THE CHICKENATOR", "Sample", 12, 12, 16);
+			return new GBRecipe(id, "THE CHICKENATOR", "A chicken dish.", 12, 12, 16);
 		case 101:
-			return new GBRecipe(id, "TIGER PRAWN PLATTER", "Sample", 34, 17, 23);
+			return new GBRecipe(id, "TIGER PRAWN PLATTER", "Beautifully presented prawn platter with 30 peeled and deveined Exmouth Tiger Prawns and a seafood cocktail sauce.", 34, 17, 23);
 		case 102:
-			return new GBRecipe(id, "TOMATO AND BASIL SOUP", "Sample", 44, 3);
+			return new GBRecipe(id, "TOMATO AND BASIL SOUP", "Sweet vine-ripened tomatoes are the key to flavor in this creamy soup.", 44, 3);
 		case 103:
-			return new GBRecipe(id, "TOMATO AND ONION TART", "Sample", 44, 28);
+			return new GBRecipe(id, "TOMATO AND ONION TART", "This meatless main dish features flaky puff pastry topped with roasted tomatoes and onion, and lots of shredded mozzarella cheese.", 44, 28);
 		case 104:
-			return new GBRecipe(id, "TUNA AND SWEETCORN PANINI", "Sample", 42,
+			return new GBRecipe(id, "TUNA AND SWEETCORN PANINI", " A panini filled with tuna and sweetcorn.", 42,
 					45, 7);
 		case 105:
-			return new GBRecipe(id, "TUNA SALAD", "Sample", 45, 29);
+			return new GBRecipe(id, "TUNA SALAD", " Tuna and mayonnaise or mayonnaise-substitute. ", 45, 29);
 		case 106:
-			return new GBRecipe(id, "TUNA STEAK", "Sample", 45, 33, 10);
+			return new GBRecipe(id, "TUNA STEAK", "A perfect candidate for grilling, and a sweet, tangy marinade keeps them moist and flavorful.", 45, 33, 10);
 		case 107:
-			return new GBRecipe(id, "TUNA SUSHI", "Sample", 36, 45, 46);
+			return new GBRecipe(id, "TUNA SUSHI", "A spicy hand roll (temaki), with avocado, cucumber and rice", 36, 45, 46);
 		case 108:
-			return new GBRecipe(id, "VEGAN TOFU DELIGHT", "Sample", 43, 5, 38);
+			return new GBRecipe(id, "VEGAN TOFU DELIGHT", "A dish filled with vegetables.", 43, 5, 38);
 		case 109:
-			return new GBRecipe(id, "VEGETABLE CHOWDER", "Sample", 25, 33, 30);
+			return new GBRecipe(id, "Vegetable Chowder", "A variety of fresh vegetables are cooked in chicken broth and combined with a creamy cheddar cheese mixture.", 25, 33, 30);
 		case 110:
-			return new GBRecipe(id, "VEGETARIAN FRIED RICE", "Sample", 36, 21,
+			return new GBRecipe(id, "Vegetarian Fried Rice", "A very flavorful fried rice with color", 36, 21,
 					26);
 		case 111:
-			return new GBRecipe(id, "VEGETARIAN PLATTER", "Sample", 44, 38, 42);
+			return new GBRecipe(id, "Vegetarian Platter", " Hummus, tabouli, feta cheese, stuffed grape leaves, calamata olives & cucumber slices", 44, 38, 42);
 		case 112:
-			return new GBRecipe(id, "VEGETARIAN SOUP NOODLE", "Sample", 27, 26,
+			return new GBRecipe(id, "Vegetarian Soup Noodle", "A bowl of rice noodles, fresh herbs, and bean sprouts swimming in a fragrant broth. ", 27, 26,
 					43);
 		case 113:
-			return new GBRecipe(id, "YAKI GYOZA", "Sample", 32, 18, 17);
+			return new GBRecipe(id, "Yaki Gyoza", "A Japanese version of Chinese dumpling.", 32, 18, 17);
 		case 114:
-			return new GBRecipe(id, "YAKI UDON", "Sample", 27, 12);
+			return new GBRecipe(id, "Yaki Udon", "Yakiudon are thick, smooth, white Japanese noodles eaten with a special sauce, meat and vegetables.", 27, 12);
 		case 115:
-			return new GBRecipe(id, "YAKIBUTA RAMEN", "Sample", 27, 32, 28);
+			return new GBRecipe(id, "Yakibuta Ramen", "Japanese noodle dish originated in China", 27, 32, 28);
 
 		default:
 			return null;
