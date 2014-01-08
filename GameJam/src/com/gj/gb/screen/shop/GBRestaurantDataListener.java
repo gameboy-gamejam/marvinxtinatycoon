ff9f78736fb253c81e3c7818f7a330cf554a1cd0
Juan Paulo Carabuena <carabuena.paulo@klab.com>
2014-01-08 18:40:54 +0800

    scaling!!!!!!!

--- GameJam/src/com/gj/gb/screen/shop/GBRestaurantDataListener.java
+++ GameJam/src/com/gj/gb/screen/shop/GBRestaurantDataListener.java
@@ -4,9 +4,13 @@ import com.gj.gb.model.GBNewCustomer;
 
 public interface GBRestaurantDataListener {
 
+	public int QUEUE_ADDED = 1;
+	
+	public int QUEUE_REMOVED = 2;
+	
 	public void onDishServed(GBNewCustomer customer);
 	
-	public void onQueueUpdate(GBNewCustomer customer);
+	public void onQueueUpdate(GBNewCustomer customer, int status);
 	
 	public void onCustomerLeft(GBNewCustomer customer);
 }
