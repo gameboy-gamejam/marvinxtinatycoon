5fafa51495830a31a0137c98349d06e7da9d09dd
Paulo Carabuena <jkishido@gmail.com>
2014-01-10 00:06:43 +0800

    remove saving

--- GameJam/src/com/gj/gb/popup/GBDaySummaryPop.java
+++ GameJam/src/com/gj/gb/popup/GBDaySummaryPop.java
@@ -1,7 +1,6 @@
 package com.gj.gb.popup;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -9,7 +8,6 @@ import android.widget.TextView;
 
 import com.gj.gb.R;
 import com.gj.gb.model.GBGameData;
-import com.gj.gb.screen.GBPopConfirm;
 import com.gj.gb.util.GBDataManager;
 
 public class GBDaySummaryPop extends Activity {
@@ -39,36 +37,7 @@ public class GBDaySummaryPop extends Activity {
 	
 	@Override
 	public void onBackPressed() {
-		Intent intent = new Intent(this, GBPopConfirm.class);
-		intent.putExtra("message", "Do you want to save the game?");
-		startActivityForResult(intent, 100);
-	}
-	
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-		
-		if (requestCode == 100) {
-			boolean save = resultCode == RESULT_OK;
-			if (save) {
-				saveData();
-			} else {
-				setResult(RESULT_OK);
-				finish();
-			}
-		} else if (requestCode == 101) {
-			setResult(RESULT_OK);
-			finish();
-		}
-	}
-
-	protected void saveData() {
-		GBDataManager.saveData();
-		
-		Intent intent = new Intent(this, GBPopConfirm.class);
-		intent.putExtra("message", "Successfully saved");
-		intent.putExtra("one_button", true);
-		intent.putExtra("btn_1", "OK");
-		startActivityForResult(intent, 101);
+		setResult(RESULT_OK);
+		finish();
 	}
 }
